b04a62e941f527b441c61a07d1172f53f0c440a6
t.artikov <t.artikov@2gis.ru>
2022-07-06 23:02:02 +0700

    SnapToMapLayout example

--- app/src/main/java/me/tartikov/dgis_map_example/MainActivity.kt
+++ app/src/main/java/me/tartikov/dgis_map_example/MainActivity.kt
@@ -2,17 +2,23 @@ package me.tartikov.dgis_map_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.dgis.sdk.ApiKeys
+import android.view.View
+import android.view.ViewGroup
 import ru.dgis.sdk.DGis
-import ru.dgis.sdk.map.MapView
+import ru.dgis.sdk.geometry.GeoPointWithElevation
+import ru.dgis.sdk.map.SnapToMapLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        DGis.initialize(applicationContext, ApiKeys(
-            map = BuildConfig.DGIS_MAP_API_KEY,
-            directory = ""
-        ))
+        DGis.initialize(applicationContext)
         super.onCreate(savedInstanceState)
-        setContentView(MapView(this))
+        setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.message).apply {
+            layoutParams = SnapToMapLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                GeoPointWithElevation(55.740444, 37.619524)
+            )
+        }
     }
 }
