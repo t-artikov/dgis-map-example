0dc5b9d3669f35a1c4610b4f6df9a8eeb6798d86
t.artikov <t.artikov@2gis.ru>
2022-07-12 09:33:38 +0700

    Custom style example

--- app/src/main/java/me/tartikov/dgis_map_example/MainActivity.kt
+++ app/src/main/java/me/tartikov/dgis_map_example/MainActivity.kt
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.dgis.sdk.ApiKeys
 import ru.dgis.sdk.DGis
+import ru.dgis.sdk.File
+import ru.dgis.sdk.coordinates.GeoPoint
+import ru.dgis.sdk.map.CameraPosition
+import ru.dgis.sdk.map.MapOptions
 import ru.dgis.sdk.map.MapView
+import ru.dgis.sdk.map.Zoom
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        DGis.initialize(applicationContext, ApiKeys(
+        val sdkContext = DGis.initialize(applicationContext, ApiKeys(
             map = BuildConfig.DGIS_MAP_API_KEY,
             directory = ""
         ))
         super.onCreate(savedInstanceState)
-        setContentView(MapView(this))
+
+        val options = MapOptions().apply {
+            position = CameraPosition(
+                GeoPoint(55.740444, 37.619524),
+                Zoom(9.0f)
+            )
+            styleFile = File.fromAsset(sdkContext, "dgis_style/style.2gis")
+        }
+        setContentView(MapView(this, options))
     }
 }
